ad0fe810e97994d44d50d434de0473ff3260814d
Veronica Stork <zealous.anemone@gmail.com>
2021-04-11 13:49:37 -0400

    Adds more tests for project function

--- Project.php
+++ Project.php
@@ -1,11 +1,13 @@
 <?php 
 
 function project($frequency, $amount) {
-  if ($frequency == "One-time" || $frequency == "Yearly"  ) {
-    return "$" . $amount;
-  }
-  elseif ($frequency == "Monthly") {
-    return "$" . $amount * 12;
+  if ($amount) {
+    if ($frequency == "One-time" || $frequency == "Yearly"  ) {
+      return "$" . $amount;
+    }
+    elseif ($frequency == "Monthly") {
+      return "$" . $amount * 12;
+    }
   }
   else {
     return "Can't calculate yearly donation.";
--- ProjectTest.php
+++ ProjectTest.php
@@ -9,10 +9,20 @@ class ProjectTest extends TestCase
         $result = project("Monthly", 10);
         $this->assertEquals("$120", $result);
     }
-    public function testYearlyu()
+    public function testYearly()
     {
         $result = project("Yearly", 120);
         $this->assertEquals("$120", $result);
+    }
+    public function testOnetime()
+    {
+        $result = project("One-time", 120);
+        $this->assertEquals("$120", $result);
+    }
+    public function testError()
+    {
+        $result = project("One-time", '');
+        $this->assertEquals("Can't calculate yearly donation.", $result);
     }
 }
 ?>
